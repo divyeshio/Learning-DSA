a9b281f6949b154b2b70dec4ab22f8f20a9df9b3
divyeshio <79130336+divyeshio@users.noreply.github.com>
2022-07-03 14:14:11 +0530

    Add diamond and pyramid pattern

---
+++ patterns/diamond.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    int spaces = 1;
+    for (int i = n - 1; i > 0; i--)
+    {
+        for (int j = 0; j < spaces; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+        spaces++;
+    }
+}
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//  *******
+//   *****
+//    ***
+//     *
