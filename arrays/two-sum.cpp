213fcf17d9cf7e57fedfa243f55cc0c376ece0f6
Divyesh Bhandari <79130336+divyeshio@users.noreply.github.com>
2022-08-22 07:13:59 +0530

    Opimize two-sum problem

--- arrays/two-sum.cpp
+++ arrays/two-sum.cpp
@@ -38,3 +38,31 @@ public:
         return {-1, -1};
     }
 }; */
+
+// 3. Optimized - Use two pointer approach
+
+/* class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        vector<pair<int, int>> v;
+        for (int i = 0; i < n; i++)
+            v.push_back({nums[i], i});
+        sort(v.begin(), v.end());
+        int s = 0, e = n - 1;
+        while (s < e)
+        {
+            int sum = v[s].first + v[e].first;
+            if (sum == target)
+                return {v[s].second, v[e].second};
+            if (sum > target)
+                e--;
+            else
+                s++;
+        }
+        return {-1, -1};
+    }
+};
+ */
